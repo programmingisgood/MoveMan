ba617d80393b00c42ecd13bf4cd25e3e3a5a36ab
Brian Cronin <programmingisgood@gmail.com>
2013-08-08 19:22:03 -0700

    Started adding arrow tiles

--- src/components.js
+++ src/components.js
@@ -1,15 +1,29 @@
 
-Crafty.c("DestroyOnCollision",
+Crafty.c("Grid",
 {
     init: function()
     {
-        this.requires("2D", "Collision");
-        var self = this;
-        this.onHit("Collision",
-	        function(hitEnts)
-	        {
-                self.trigger("CollisionDestroy");
-	            self.destroy();
-	        });
+        this.requires("2D");
+        this.gridX = 0;
+        this.gridY = 0;
+        this.gridWidth = 0;
+        this.gridHeight = 0;
+        this.tileWidth = 0;
+        this.tileHeight = 0;
+    },
+
+    grid: function(gridWidth, gridHeight, tileWidth, tileHeight)
+    {
+        this.gridWidth = gridWidth;
+        this.gridHeight = gridHeight;
+        this.tileWidth = tileWidth;
+        this.tileHeight = tileHeight;
+    },
+
+    setGridPos: function(tileX, tileY)
+    {
+        this.gridX = tileX;
+        this.gridY = tileY;
+
     }
 });
--- src/scene_game.js
+++ src/scene_game.js
@@ -1,10 +1,32 @@
 
 var gameGraphics = [ "assets/moveman.png", "assets/tile_blank.png", "assets/tile_start.png" ];
-var tileGraphics = { B: "assets/tile_blank.png", S: "assets/tile_start.png" };
+var tileGraphics = { B: "assets/tile_blank.png", S: "assets/tile_start.png", A: "assets/tile_start.png" };
+
+var kTileLogics = { }
+kTileLogics.A = {
+    OnEnter: function(tile, ent) {
+
+        if (ent.has("moveman"))
+        {
+            ent.dirX = tile.dirX;
+            ent.dirY = tile.dirY;
+        }
+    } };
+
 levels = new Array();
 
+var kMovemanWidth = 16;
+var kMovemanHeight = 32;
 var kTileWidth = 32;
 var kTileHeight = 32;
+
+function GetTileAt(level, x, y)
+{
+    var atTileX = Math.floor(x / kTileWidth);
+    var atTileY = Math.floor(y / kTileHeight);
+    var numDown = level.height;
+    return level.tiles[numDown * atTileY + atTileX];
+}
 
 function LoadLevel(level, moveman)
 {
@@ -12,6 +34,7 @@ function LoadLevel(level, moveman)
     var height = level.height;
     var x = 0;
     var y = 0;
+    var tiles = new Array();
     for (var t = 0; t < level.tiles.length; t++)
     {
         var tileType = level.tiles[t];
@@ -19,9 +42,32 @@ function LoadLevel(level, moveman)
                    .attr({ x: x, y: y, z: 0, w: kTileWidth, h: kTileHeight })
                    .image(tileGraphics[tileType])
 
+        tile.logic = kTileLogics[tileType];
         if (tileType == "S")
         {
-            moveman.attr({ x: x + moveman.w / 2, y: y - kTileHeight / 2});
+            moveman.attr({ x: x + moveman.w / 2, y: y - kTileHeight / 2 });
+        }
+        else if (tileType == "A")
+        {
+            tile.dirX = 0;
+            tile.dirY = 0;
+            var leftRight = Math.random() > 0.5;
+            if (leftRight)
+            {
+                var left = Math.random() > 0.5;
+                if (left)
+                    tile.dirX = -1
+                else
+                    tile.dirX = 1;
+            }
+            else
+            {
+                var up = Math.random() > 0.5;
+                if (up)
+                    tile.dirY = 1;
+                else
+                    tile.dirY = -1;
+            }
         }
 
         x += kTileWidth;
@@ -30,19 +76,23 @@ function LoadLevel(level, moveman)
             x = 0;
             y += kTileHeight;
         }
+        tiles.push(tile);
     }
+    return { width: level.width, height: level.height, tiles: tiles };
 }
 
 Crafty.scene('Game', function()
 {
-    Crafty.background("rgb(190, 190, 190)");
+    Crafty.background("rgb(80, 80, 80)");
 
     Crafty.load(gameGraphics);
+
+    var currentLevel = null;
 
     Crafty.sprite("assets/moveman.png", { moveman: [ 0, 0, 64, 32 ] } );
 
     var moveman = Crafty.e("2D, Canvas, Collision, Delay, moveman")
-                  .attr({ x: Game.width() / 2, y: Game.height() / 2, z: 1, w: 16, h: 32 })
+                  .attr({ x: Game.width() / 2, y: Game.height() / 2, z: 1, w: kMovemanWidth, h: kMovemanHeight })
                   .collision();
 
     moveman.sprite(0, 0, 16, 32);
@@ -67,7 +117,14 @@ Crafty.scene('Game', function()
             var x = kTileWidth * this.dirX;
             var y = kTileHeight * this.dirY;
             this.shift(x, y);
+            var translatedX = this.x - this.w / 2;
+            var translatedY = this.y + kTileHeight / 2; 
+            var enterTile = GetTileAt(currentLevel, translatedX, translatedY);
+            if (enterTile && enterTile.logic)
+            {
+                enterTile.logic.OnEnter(enterTile, this);
+            }
         }, 1000, -1);
 
-    LoadLevel(levels[0], moveman);
+    currentLevel = LoadLevel(levels[0], moveman);
 });
